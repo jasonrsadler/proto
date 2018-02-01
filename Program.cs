ad208f6c3e02277e0ae4e24901d7324b581a32c0
Jason Sadler <jason.r.sadler@gmail.com>
2018-01-31 22:19:00 -0500

    Proto complete

--- Program.cs
+++ Program.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Text;
 namespace proto
 {
+    
     class Program
     {
         static void Main(string[] args)
@@ -13,34 +14,67 @@ namespace proto
             Double totalDebits = 0.0f;
             Double totalCredits = 0.0f;
             Int32 numAutoPaysStarted = 0;
-            Int32 numAutoPaysEnded = 0;
+            Int32 numAutoPaysEnded = 0;            
             Double balanceOfSpecifiedUser = 0.0f;
             const UInt64 specifiedUser = 2456938384156277127;
             Console.WriteLine("Reading File.....");
-            BinaryReader reader = new BinaryReader(File.Open("./proto/txnlog.dat", FileMode.Open));            
+            BinaryReader reader = new BinaryReader(File.Open("./txnlog.dat", FileMode.Open));            
             //read in the first 'line' of bytes for header            
             String inputType = Convert.ToString(Encoding.ASCII.GetString(reader.ReadBytes(4).ToArray())); //magic, don't need to resolve
             String version = BitConverter.ToString(reader.ReadBytes(1).ToArray()); //version            
             UInt32 numRecords = (UInt32)ResolveNetworkBytes(reader, true);          
+            
+            Console.WriteLine(inputType + "\n" + version + "\n" + numRecords.ToString());
+                
             //read in records
             for (int ix = 0; ix < numRecords; ix++) {
-                String recordType = BitConverter.ToString(reader.ReadBytes(1).ToArray()); //1 byte just read in
+                RecordTypes recordType = (RecordTypes)Convert.ToInt32
+                    (BitConverter.ToString(reader.ReadBytes(1).ToArray())); //1 byte just read in
                 UInt32 timeStamp = (UInt32)ResolveNetworkBytes(reader, true);
-                UInt64 userID = ResolveNetworkBytes(reader, false);
+                UInt64 userID = ResolveNetworkBytes(reader, false);                
                 Double amount = 0.0f;
-                if (recordType == "00" || recordType == "01") {
-                    //read in more
-                    amount = ResolveNetworkBytes(reader);
-                    if (recordType == "00")
-
+                //read in additional bytes of debit or credit
+                    
+                switch(recordType) {
+                    case RecordTypes.Credit: {
+                        amount = ResolveNetworkBytes(reader);
+                        totalCredits += amount;
+                        if (userID == specifiedUser) {
+                            balanceOfSpecifiedUser = amount;
+                        }
+                        break;
+                    } 
+                    case RecordTypes.Debit: {
+                        amount = ResolveNetworkBytes(reader);
+                        totalDebits += amount;
+                        if (userID == specifiedUser) {
+                            balanceOfSpecifiedUser = amount;
+                        }
+                        break;
+                    } 
+                    case RecordTypes.StartAutoPay: {
+                        numAutoPaysStarted++;
+                        break;
+                    }
+                    case RecordTypes.EndAutoPay: {
+                        numAutoPaysEnded++;
+                        break;
+                    }
                 }
+
+                
                 
                 //print results to stdout
-                Console.WriteLine(inputType + "\n" + version + "\n" + numRecords.ToString());
                 Console.WriteLine("Record is type: " + recordType + " timestamp: " + timeStamp.ToString() + 
-                    " userID: " + userID.ToString() + " amount: " + String.Format("{0:0.00}", Convert.ToDecimal(amount.ToString())));
-                Console.WriteLine("=====================================\nAnswers To Questions:\n=====================================");
+                    " userID: " + userID.ToString() + " amount: " + String.Format("{0:0.00}", Convert.ToDecimal(amount.ToString())));                
             }
+            Console.WriteLine("=====================================\nAnswers To Questions:\n=====================================");
+            Console.WriteLine("Total Debits: $" + String.Format("{0:0.00}", Convert.ToDecimal(totalDebits)));
+            Console.WriteLine("Total Credits: $" + String.Format("{0:0.00}", Convert.ToDecimal(totalCredits)));
+            Console.WriteLine("AutoPays Started: " + numAutoPaysStarted.ToString());
+            Console.WriteLine("AutoPays Ended: " + numAutoPaysEnded.ToString());
+            Console.WriteLine("Balance of UserID " + specifiedUser.ToString() + ": " + String.Format("{0:0.00}", Convert.ToDecimal(balanceOfSpecifiedUser)));
+            Console.WriteLine("=====================================\nEND\n=====================================");
         }
         static UInt64 ResolveNetworkBytes(BinaryReader reader, bool IsUnsigned32) {
             byte[] bytes = IsUnsigned32 ? reader.ReadBytes(4) : reader.ReadBytes(8);
@@ -57,4 +91,8 @@ namespace proto
                 Array.Reverse(valueBytes);
         }
     }
+
+    enum RecordTypes {
+        Debit = 0, Credit = 1, StartAutoPay = 2, EndAutoPay = 3
+    };
 }
